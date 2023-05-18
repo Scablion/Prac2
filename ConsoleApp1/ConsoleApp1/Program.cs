5c1dbc290d8ba6815ff75b02832e14975707a05e
Scablion <kashcada24@gmail.com>
2023-05-18 22:09:53 +0300

    add First exercise

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите 3х значное число: ");
+            string num = Console.ReadLine();
+            string reverse = "";
+            reverse += num[1];
+            reverse += num[2];
+            reverse += num[0];
+            Console.WriteLine(reverse);
+        }
+    }
+}
