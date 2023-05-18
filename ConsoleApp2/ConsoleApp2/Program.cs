934f55d802aae75ea41d7d748c7360d92c3051bf
Scablion <kashcada24@gmail.com>
2023-05-18 22:20:56 +0300

    Complete second number

--- ConsoleApp2/ConsoleApp2/Program.cs
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -10,6 +10,12 @@ namespace ConsoleApp2
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("3х значное число:");
+            int num = int.Parse(Console.ReadLine());
+            int change = num % 10;
+            change += num / 100 * 10;
+            change += num / 10 % 10 * 100;
+            Console.WriteLine(change);
         }
     }
 }
