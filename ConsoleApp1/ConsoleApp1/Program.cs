355191b757d86e6a364e5d394f1ff121a9ce771a
Scablion <kashcada24@gmail.com>
2023-05-20 08:42:48 +0300

    project

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите 3х значное число: ");
+            int num = int.Parse(Console.ReadLine());
+            int reverse = num % 100;
+            reverse = num / 100 + reverse * 10;
+            Console.WriteLine(reverse);
+        }
+    }
+}
